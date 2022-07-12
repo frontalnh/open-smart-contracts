2af1d4a29639d19c6c6e06f40ceed04feaea3459
Namhoon Lee <frontalnh@gmail.com>
2022-07-12 18:42:40 +0900

    update

--- ethereum/GeneralERC20/contracts/GeneralERC20ProxyAdmin.sol
+++ ethereum/GeneralERC20/contracts/GeneralERC20ProxyAdmin.sol
@@ -3,4 +3,4 @@
 pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
-contract FootageProxyAdmin is ProxyAdmin {}
+contract GeneralERC20ProxyAdmin is ProxyAdmin {}
